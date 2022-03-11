c1b8d4c8a0f9372fa9b2b3de5ec082e6f7bd8bcb
Dennisops <itsdennis254@gmail.com>
2022-03-11 13:55:18 +0000

    Output fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
   */
 int main(void)
 {
-	printf("Size of char: %lu byte(s) \n", sizeof(char));
+	printf("Size of a char: %lu byte(s) \n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of long int: %lu byte(s) \n", sizeof(long int));
 	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
