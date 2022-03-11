6dc73a5b889ca80f9ed038c34abb8dbfc1a99909
Dennisops <itsdennis254@gmail.com>
2022-03-11 14:34:18 +0000

    quote program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Contains printf function with a string
+  *
+  * Return: Always 1 (Success)
+  */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (0);
+}
