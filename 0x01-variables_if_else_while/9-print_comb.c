fb428b83c89cb241c4124924ed661dab76635687
Dennisops <itsdennis254@gmail.com>
2022-03-13 15:51:39 +0000

    A program that prints characters with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - The function prints single digit numbers with commas
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+		if (x == 9)
+			continue;
+		else
+			putchar(',');
+	}
+	putchar('$');
+	putchar('\n');
+	return (0);
+}
