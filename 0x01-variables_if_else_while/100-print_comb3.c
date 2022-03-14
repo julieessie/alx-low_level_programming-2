412dc49d5939107fb7980968fc2a1aa136433407
Dennisops <itsdennis254@gmail.com>
2022-03-14 10:44:26 +0000

    two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - The program prints two digit numbers with no repetition
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '8' ; x++)
+		for (y = '0'; y <= '9'; y++)
+			if (x < y)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != '8' || (x == '8' && y != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+	putchar('\n');
+
+	return (0);
+}
