f74df055e906d9c4d0e8e2abcf1f1c6eeecafcc1
Dennisops <itsdennis254@gmail.com>
2022-03-17 09:07:06 +0000

    prints all minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * jack_bauer - Prints every minute of the day
+  *
+  * Return: Always 0 (success)
+  */
+void jack_bauer(void)
+{
+	int hrs;
+	int min;
+
+	for (hrs = 0; hrs <= 23; hrs++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n')
+		}
+	}
+}
+
