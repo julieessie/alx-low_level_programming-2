829586c2e0e05ab532fceee7f05105f90a720958
Dennisops <itsdennis254@gmail.com>
2022-03-17 12:56:02 +0000

    added a new folder for debugging tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,18 +7,30 @@
   */
 void times_table(void)
 {
-	int x;
-	int y;
+	int x, y, z, num;
 
-	for (x = 0; x <= 9; x++)
+	for (y = 1; y <= 10; y++)
 	{
-		_putchar(' ');
-		for (y = 1; y <= 9; y++)
+		for (x = 0; x <= 9; x++)
 		{
-			int num = x*y;
+			for (z = 0; z <= 9; z++)
+				num = z * y;
 			_putchar(num + '0');
-			_putchar(',');
+			if (x == 9)
+				continue;
+			else
+				_putchar(',');
 			_putchar(' ');
+			if (x == 0)
+				_putchar(' ');
+			else
+				continue;
 		}
+	_putchar('\n');
 	}
+}
+int main(void)
+{
+	times_table(void);
+	return (0);
 }
