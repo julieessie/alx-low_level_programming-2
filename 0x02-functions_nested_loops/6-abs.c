fcbbe8cb0e89b13bb77a783ef3851b115b536287
Dennisops <itsdennis254@gmail.com>
2022-03-17 06:55:22 +0000

    program to change integer to absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _abs - Computes the value of an integer
+  * @int: integer to be checked
+  *
+  * Return: Always 0 (success)
+  */
+int _abs(int)
+{
+	int x;
+
+	if (x < 0)
+	{
+		return (x * -1);
+	}
+	else if (x == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (x);
+	}
+}
