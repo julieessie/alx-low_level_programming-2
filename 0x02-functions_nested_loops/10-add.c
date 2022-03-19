1f05ac9ce46826eed4bf34fd4dfbfbdcccb06eaf
Dennisops <itsdennis254@gmail.com>
2022-03-19 12:13:55 +0000

    A function that returns the sum of the integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * add - Adds two integers
+  *@x: the first operand
+  *@y: the second operand
+  *
+  * Return: The sum of the integers
+  */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -39,3 +39,9 @@ void times_table(void)
 	_putchar('\n');
 	}
 }
+int main(void)
+{
+	times_table();
+	return 0;
+}
+
