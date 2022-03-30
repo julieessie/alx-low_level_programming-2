d5a7fb6241801c86d470d86e0831b3d9f3d10778
Dennisops <itsdennis254@gmail.com>
2022-03-30 15:55:39 +0000

    function that swaps the integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps two integers using dereferencing
+ * @a: The first parameter to be swapped
+ * @b: The second parameter to be swapped
+ *
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
