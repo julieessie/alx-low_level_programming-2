e42287e774aa8b0443b16d11db139edc08d3312a
Dennisops <itsdennis254@gmail.com>
2022-03-30 15:44:40 +0000

    changes the value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value of n to 98 through dereferencing
+ * @n: The parameter to be changed
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
