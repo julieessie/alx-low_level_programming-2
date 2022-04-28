cf1a2cf165fa117bb9a8bdfc7eb380e3253c8155
Dennisops <itsdennis254@gmail.com>
2022-04-28 19:30:45 +0000

    binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,84 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * str_len - Finds the length of string
+ *
+ * @s: String of which length is to be found.
+ *
+ * Return: The length of the string
+ */
+int str_len(const char *s)
+{
+	int len = 0, idx;
+
+	for (idx = 0; *(s + idx); idx++)
+	{
+		if (*(s + idx) == '\0')
+			break;
+		len++;
+	}
+	return (len);
+}
+/**
+ * power - Finds power @y of @x.
+ *
+ * @x: Base number.
+ * @y: Exponent
+ *
+ * Return: x raised to power y
+ */
+int power(int x, int y)
+{
+	int pow = 0;
+
+	if (y == 0)
+		return (1);
+	pow = x * power(x, y - 1);
+
+	return (pow);
+}
+/**
+ * binary_to_uint - converts the binary number to an unsigned int
+ * @b: The string to be checked
+ *
+ * Return: 0 if there are one or more characters in the string b or b is null
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len = str_len(b);
+	unsigned int dec = 0, idx = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (len > 0)
+	{
+		--len;
+		if (*(b + len) != '0' && *(b + len) != '1')
+			return (0);
+
+		dec += (*(b + len) - '0') * power(2, idx);
+		idx++;
+	}
+	return (dec);
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned int n;
+
+	n = binary_to_uint("1");
+	printf("%u\n", n);
+	n = binary_to_uint("101");
+	printf("%u\n", n);
+	n = binary_to_uint("1e01");
+	printf("%u\n", n);
+	n = binary_to_uint("1100010");
+	printf("%u\n", n);
+	return (0);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,10 @@
+# Bit-Manipulation
+
+This project looks into bit manipulation with operators such as &, ^, |, << and >>
+
+Where & represents 'and' which means the result will always be 1 if both sides are 1
+Where | represents 'or' which means the result will always be 1 if there is 1 on either side
+Where ^ represents 'Xor' (Exclusive or) which means the result will be 1 only if one side has 1
+Where ~ represents 'inverse' which means the output will be the inverse of the digit i.e. 1 becomes 0 and 0 becomes 1
+where >> shifts the number to the right according to the bits indicated
+where << shifts the number to the left according to the bits inicated
