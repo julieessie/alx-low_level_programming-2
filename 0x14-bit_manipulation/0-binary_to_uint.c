e6b3a6e494fabd65b0e87feb470a54a6993a2427
Dennisops <itsdennis254@gmail.com>
2022-04-28 19:33:21 +0000

    removed the main function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -63,22 +63,3 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (dec);
 }
-/**
- * main - check the code
- *
- * Return: Always 0
- */
-int main(void)
-{
-	unsigned int n;
-
-	n = binary_to_uint("1");
-	printf("%u\n", n);
-	n = binary_to_uint("101");
-	printf("%u\n", n);
-	n = binary_to_uint("1e01");
-	printf("%u\n", n);
-	n = binary_to_uint("1100010");
-	printf("%u\n", n);
-	return (0);
-}
